1cf3ee2d27fb08139b8366c91cde742166f448e5
Htoo Maung Maung <htoomaungmg@gmail.com>
2019-06-09 00:57:34 +0800

    added react-toastify for notification message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Donation from './containers/donation';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
   componentDidMount () {
@@ -10,6 +12,7 @@ class App extends Component {
     return (
       <div>
         <Donation/>
+        <ToastContainer autoClose={5000} position={`top-center`} />
       </div>
     );
   }
